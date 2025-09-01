a284022b3aa84e7346b10d80e57e61d055935683
BlackShima <Blackshima02@gmail.com>
2025-09-02 00:24:08 +0700

    Add files via upload

---
+++ Chapter4/src/main/java/se233/chapter4/controller/GameLoop.java
@@ -0,0 +1,65 @@
+package se233.chapter4.controller;
+
+import se233.chapter4.model.GameCharacter;
+import se233.chapter4.view.GameStage;
+
+public class GameLoop implements Runnable {
+    private GameStage gameStage;
+    private int frameRate;
+    private float interval;
+    private boolean running;
+    public GameLoop(GameStage gameStage) {
+        this.gameStage = gameStage;
+        frameRate = 50;
+        interval = 1000.0f / frameRate;
+        running = true;
+    }
+    private void update(GameCharacter gameCharacter) {
+        boolean leftPressed = gameStage.getKeys().isPressed(gameCharacter.getLeftKey());
+        boolean rightPressed = gameStage.getKeys().isPressed(gameCharacter.getRightKey());
+        boolean upPressed = gameStage.getKeys().isPressed(gameCharacter.getUpKey());
+
+        gameCharacter.stop();
+
+        if (leftPressed && rightPressed) {
+
+        } else if (leftPressed) {
+            gameCharacter.getImageView().tick();
+            gameCharacter.moveLeft();
+        } else if (rightPressed) {
+            gameCharacter.getImageView().tick();
+            gameCharacter.moveRight();
+        } else {
+            gameCharacter.stop();
+        }
+
+        if (upPressed) {
+            gameCharacter.jump();
+        }
+        gameCharacter.checkReachGameWall();
+        gameCharacter.checkReachHighest();
+        gameCharacter.checkReachFloor();
+        gameCharacter.repaint();
+    }
+    @Override
+    public void run() {
+        while (running) {
+            float time = System.currentTimeMillis();
+            update(gameStage.getGameCharacter());
+            time = System.currentTimeMillis() - time;
+            if (time < interval) {
+                try {
+                    Thread.sleep((long) (interval - time));
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                try {
+                    Thread.sleep((long) (interval - (interval % time)));
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
